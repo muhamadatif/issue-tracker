4eb31212c8640ff0c672cccaa76cf11315b9e8a0
Mohamed Atif <mohamedabotreka40@gmail.com>
2024-07-10 00:06:44 +0300

    Build the new issue page

---
+++ app/issues/new/page.tsx
@@ -0,0 +1,20 @@
+// Beacuse we interacting with  a form which happen on the browser we need to use use client
+
+"use client";
+
+import { Button, TextArea, TextField } from "@radix-ui/themes";
+import React from "react";
+
+const NewIssuePage = () => {
+  return (
+    <div className="max-w-xl space-y-3">
+      <TextField.Root>
+        <TextField.Input placeholder="Title" />
+      </TextField.Root>
+      <TextArea placeholder="Description" />
+      <Button>Submit New Issue</Button>
+    </div>
+  );
+};
+
+export default NewIssuePage;
