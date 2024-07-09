a519c07f8cdf78f985e11a02509b6dde1f1b106d
Mohamed Atif <mohamedabotreka40@gmail.com>
2024-07-09 22:59:41 +0300

    Building API for creating issues

---
+++ prisma/client.ts
@@ -0,0 +1,17 @@
+//This code create an instanse of prisma client and insure that there is only single instanse
+
+import { PrismaClient } from "@prisma/client";
+
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
+
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+
+export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
