6d233c9945ce99ced91d4fadc8e7b404a97bafde
Mohamed Atif <mohamedabotreka40@gmail.com>
2024-07-11 23:07:51 +0300

    Add the Prisma Adapter

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,20 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "@/prisma/client";
 const handler = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
+
+  // next-auth change the auth from jwt to server but Oauth doesn't work with google provider so we need to manually chagne the authentication to jwt
+  session: {
+    strategy: "jwt",
+  },
 });
 
 export { handler as GET, handler as POST };
