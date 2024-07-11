c3313f698ed3431e16f038078744490e4f1be9bf
Mohamed Atif <mohamedabotreka40@gmail.com>
2024-07-11 17:00:02 +0300

    Add a delete button

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,13 +1,15 @@
 import prisma from "@/prisma/client";
-import { Box, Grid } from "@radix-ui/themes";
+import { Box, Flex, Grid } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
 import EditIssueButton from "./EditIssueButton";
 import IssueDetails from "./IssueDetails";
+import DeleteIssueButton from "./DeleteIssueButton";
 
 // interface Props {
 //   params: { id: string };
 // }
 
+//sm in radix equal to md in tailwind
 const IssueDetailPage = async ({ params }: { params: { id: string } }) => {
   const issue = await prisma.issue.findUnique({
     where: {
@@ -19,15 +21,18 @@ const IssueDetailPage = async ({ params }: { params: { id: string } }) => {
     <Grid
       columns={{
         initial: "1",
-        md: "2",
+        sm: "5",
       }}
       gap="5"
     >
-      <Box>
+      <Box className="md:col-span-4">
         <IssueDetails issue={issue} />
       </Box>
       <Box>
-        <EditIssueButton issueId={issue.id} />
+        <Flex direction="column" gap="4">
+          <EditIssueButton issueId={issue.id} />
+          <DeleteIssueButton issueId={issue.id} />
+        </Flex>
       </Box>
     </Grid>
   );
