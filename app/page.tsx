694919ebc26c898bb3913e44ba7c78769eef0ce2
Mohamed Atif <mohamedabotreka40@gmail.com>
2024-07-16 17:43:56 +0300

    Build the LatesIssue component

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,10 @@
 import Pagination from "./components/Pagination";
+import LatestIssues from "./LatestIssues";
 
 export default function Home({
   searchParams,
 }: {
   searchParams: { page: string };
 }) {
-  return (
-    <div>
-      <Pagination
-        itemCount={100}
-        pageSize={10}
-        currentPage={+searchParams.page}
-      />
-    </div>
-  );
+  return <LatestIssues />;
 }
